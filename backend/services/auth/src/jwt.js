484e21f5c752fa9a381d40781ec551d07288d2a8
Sheratan095 <marco.ceccarelli.code@gmail.com>
2025-10-30 15:35:16 +0100

    todos

--- backend/services/auth/src/jwt.js
+++ backend/services/auth/src/jwt.js
@@ -77,7 +77,7 @@ export function	setAuthCookies(reply, newTokens)
 	{
 		path: '/',
 		httpOnly: true,
-		secure: true, // For http TO DO set "true" in production with https
+		secure: true,
 		sameSite: 'none', // Frontend and backend are on different domains
 		maxAge: accessTokenMaxAge
 	});
@@ -86,7 +86,7 @@ export function	setAuthCookies(reply, newTokens)
 	{
 		path: '/',
 		httpOnly: true,
-		secure: true, // For http TO DO set "true" in production with https
+		secure: true,
 		sameSite: 'none', // Frontend and backend are on different domains
 		maxAge: refreshTokenMaxAge
 	});
