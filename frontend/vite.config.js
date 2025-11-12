074741c84c2dc0615b01d58ed8e5e0ac0e59caaf
John Doe <johndoe@example.com>
2025-11-12 18:41:25 +0100

    asss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   base: '/',
@@ -15,4 +17,28 @@ export default defineConfig({
     }
   },
   publicDir: 'public',
+  plugins: [
+		tailwindcss(),
+
+		{
+			name: 'reload-on-frontend-ts',
+			handleHotUpdate({ file, server }) {
+				const relativePath = path.relative(process.cwd(), file);
+				const isInFrontend = relativePath.startsWith('frontend' + path.sep);
+
+				const validExtensions = ['.ts', '.js', '.html', '.css'];
+				const hasValidExtension = validExtensions.some(ext => file.endsWith(ext));
+
+				if (isInFrontend && hasValidExtension) {
+					console.log(`[vite] Change detected in ${relativePath}. Triggering full page reload.`);
+					server.ws.send({
+						type: 'full-reload',
+						path: '*',
+					});
+				}
+
+				return [];
+			},
+		},
+	],
 })
