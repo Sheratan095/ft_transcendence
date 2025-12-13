b7f3c4a099c07d66064f478ad75734c0b27049bf
Adam2475 <adamnatanpassaretti@gmail.com>
2025-12-13 19:43:19 +0100

    added board click recognition and started turn logic

--- backend/services/tris/src/tris-controllers.js
+++ backend/services/tris/src/tris-controllers.js
@@ -1,7 +1,7 @@
 
 export const initBoard = async (req, reply) =>
 {
-    console.log("Initializing board");
+    //console.log("Initializing board");
     const board = Array(9).fill('empty');
     return reply.code(200).send(board);
 }
