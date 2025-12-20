4064d5ebe0fb3795ea7af23748568052eae975b6
Adam2475 <adamnatanpassaretti@gmail.com>
2025-12-20 21:28:24 +0100

    finished check victory logic

--- backend/services/tris/src/tris-controllers.js
+++ backend/services/tris/src/tris-controllers.js
@@ -2,7 +2,11 @@
 export const initBoard = async (req, reply) =>
 {
     //console.log("Initializing board");
-    const board = Array(9).fill('empty');
+    //const board = Array.from({ length: 3 }, () => Array(3).fill('empty'));
+    const board = Array(3);
+    board[0] = Array("empty", "empty", "empty");
+    board[1] = Array("empty", "empty", "empty");
+    board[2] = Array("empty", "empty", "empty");
     return reply.code(200).send(board);
 }
 
